b7e9ffff0a5f63ccf13b63e7df29db1ac9f3359e
Nestor Garcia <nestorjgcc@hotmail.com>
2022-05-04 18:36:11 -0700

    Button made.

--- index.js
+++ index.js
@@ -8,28 +8,29 @@ document.querySelector(".dice .img1").setAttribute("src", image1);
  */
 
 //Create a number.
+document.querySelector(".playButton").addEventListener("click", function () {
+  let randomNumber1 = Math.random() * 6;
 
-let randomNumber1 = Math.random() * 6;
+  randomNumber1 = Math.floor(randomNumber1) + 1;
 
-randomNumber1 = Math.floor(randomNumber1) + 1;
+  let image1 = "images/dice" + randomNumber1 + ".png";
 
-let image1 = "images/dice" + randomNumber1 + ".png";
+  document.querySelector(".dice .img1").setAttribute("src", image1);
 
-document.querySelector(".dice .img1").setAttribute("src", image1);
+  let randomNumber2 = Math.random() * 6;
 
-let randomNumber2 = Math.random() * 6;
+  randomNumber2 = Math.floor(randomNumber2) + 1;
 
-randomNumber2 = Math.floor(randomNumber2) + 1;
+  let image2 = "images/dice" + randomNumber2 + ".png";
 
-let image2 = "images/dice" + randomNumber2 + ".png";
+  document.querySelector(".dice .img2").setAttribute("src", image2);
 
-document.querySelector(".dice .img2").setAttribute("src", image2);
-
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "Play 1 Wins!";
-}
-if (randomNumber1 < randomNumber2) {
-  document.querySelector("h1").innerHTML = "Play 2 Wins!";
-} else if (randomNumber1 === randomNumber2) {
-  document.querySelector("h1").innerHTML = "Draw!";
-}
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Nestor Wins!";
+  }
+  if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Guest Wins!";
+  } else if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
+});
